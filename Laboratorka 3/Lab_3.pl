5e89322a6ddb9ae7980f55ec4bf211105786dddf
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-02-26 18:09:54 +0300

    Task 5

--- Laboratorka 3/Lab_3.pl
+++ Laboratorka 3/Lab_3.pl
@@ -9,3 +9,7 @@ fact(N,X):- N1 is N - 1,fact(N1,X1),X is X1*N.
 fact_down(N,X):- fact_down(0,1,N,X).
 fact_down(N,X,N,X):-!.
 fact_down(I,C,N,X):- I1 is I + 1, C1 is C * I1, fact_down(I1,C1,N,X).
+
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):- N1 is N - 1, N2 is N - 2, fib(N1,X1),fib(N2,X2),X is X1 + X2.
