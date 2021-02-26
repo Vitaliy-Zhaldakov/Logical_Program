59d6b041e211ea6a4ac17f8e5c47e75e2d199c6a
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-02-26 17:53:25 +0300

    Task 4

--- Laboratorka 3/Lab_3.pl
+++ Laboratorka 3/Lab_3.pl
@@ -5,3 +5,7 @@ max(X,Y,U,Z):- max(X,Y,C), max(U,C,Z).
 
 fact(0,1):-!.
 fact(N,X):- N1 is N - 1,fact(N1,X1),X is X1*N.
+
+fact_down(N,X):- fact_down(0,1,N,X).
+fact_down(N,X,N,X):-!.
+fact_down(I,C,N,X):- I1 is I + 1, C1 is C * I1, fact_down(I1,C1,N,X).
