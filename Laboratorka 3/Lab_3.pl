3bd7914337bcd0e7f0980c5acc402a2bb4006e41
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-02-27 00:18:36 +0300

    Task 12

--- Laboratorka 3/Lab_3.pl
+++ Laboratorka 3/Lab_3.pl
@@ -37,11 +37,26 @@ max_num_down(N,Curr_max,Max):- N1 is N mod 10, N2 is N div 10,
 
 min_nechet_up(0,10):-!.
 min_nechet_up(N,X):- N1 is N div 10, min_nechet_up(N1,Min1),
-    N2 is N mod 10, ((N2 < Min1, 0 \= N2 mod 2) -> X = N2 ;
+    N2 is N mod 10, ((N2 < Min1, N3 is N2 mod 2, N3 \= 0) -> X = N2 ;
                   X = Min1).
 
 min_nechet_down(N,X):- min_nechet_down(N,10,X).
+min_nechet_down(0,10,_):-!.
 min_nechet_down(0,X,X):-!.
 min_nechet_down(N,Curr_min,X):- N1 is N mod 10, N2 is N div 10,
-    (   (N1 < Curr_min, 0 \= N1 mod 2) -> Min1 = N1 ; Min1 = Curr_min),
+    (   (N1 < Curr_min, N3 is N1 mod 2, N3 \= 0) -> Min1 = N1 ; Min1 = Curr_min),
     min_nechet_down(N2,Min1,X).
+
+nod(X,Y,Nod):- nod(X,Y,0,1,Nod).
+nod(_,Y,Y,Nod,Nod):-!.
+nod(X,_,X,Nod,Nod):-!.
+nod(X,Y,I,Cur_nod,Nod):- I1 is I + 1,((0 is X mod I1, 0 is Y mod I1) ->
+           Cur_nod1 = I1 ; Cur_nod1 = Cur_nod),nod(X,Y,I1,Cur_nod1,Nod).
+
+is_simple(X):- is_simple(X,2).
+is_simple(X,X):-!.
+is_simple(X,I):- X1 is X mod I, X1 \= 0, I1 is I + 1, is_simple(X,I1).
+
+kol_del(N,Count):- kol_del(N,N,Count).
+kol_del(1,_,1):-!.
+kol_del(I,N,Count):- I1 is I - 1, kol_del(I1,N,C),(0 is N mod I -> Count is C + 1 ;Count is C).
