b81f6acc4e2026a8c306f0bb7dd67cf3fb9f9975
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-03-03 20:26:12 +0300

    Task 4

--- Laboratorka 4/Lab_4.pl
+++ Laboratorka 4/Lab_4.pl
@@ -11,3 +11,11 @@ proga_summ :- write("Введите число элементов: "), read(N), read_list(N,List),
 
 sum_list_up([],0):-!.
 sum_list_up([H|T],Sum):- sum_list_up(T,S1), Sum is H + S1.
+
+list_el_numb(List,Elem,Number):- list_el_numb(List,Elem,0,Number).
+list_el_numb([H|_],H,Number,Number):-!.
+list_el_numb([_|T],Elem,I,Number):- I1 is I + 1, list_el_numb(T,Elem,I1,Number).
+proga_elem :- write("Введите число элементов: "), read(N), read_list(N,List),
+    write("Введите элемент: "), read(Elem), nl, list_el_numb(List,Elem,Number),
+    write("Позиция элемента: "), write(Number),!.
+proga_elem :- write("Такого элемента нет").
