dbd975665304ce019e9e914f602a625d67f53ab2
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-03-06 02:40:11 +0300

    Task 2

---
+++ Laboratorka 2/Akinator.pl
@@ -0,0 +1,362 @@
+character(luke_skywalker,0).
+character(c-зpo,1).
+character(r2-d2,1).
+character(princess_leia,0).
+character(yoda,2).
+character(darth_vader,0).
+character(darth_sidious,0).
+character(padme_amidalla,0).
+character(han_solo,0).
+character(anakin_skywalker,0).
+character(obi-wan_kenobi,0).
+character(rey,0).
+character(darth_maul,0).
+character(boba_fett,0).
+character(jango_fett,0).
+character(general_grievous,2).
+character(ja-ja_binks,2).
+character(chewbacca,2).
+character(finn,0).
+character(moff_gideon,0).
+character(grand_moff_tarkin,0).
+character(ahsoka_tano,2).
+character(mace_windu,0).
+character(admiral_akbar,2).
+character(mandalorian,0).
+
+side(luke_skywalker,1).
+side(c-зpo,1).
+side(r2-d2,1).
+side(princess_leia,1).
+side(yoda,1).
+side(darth_vader,0).
+side(darth_sidious,0).
+side(padme_amidalla,1).
+side(han_solo,1).
+side(anakin_skywalker,1).
+side(obi-wan_kenobi,1).
+side(rey,1).
+side(darth_maul,0).
+side(boba_fett,0).
+side(jango_fett,0).
+side(general_grievous,0).
+side(ja-ja_binks,1).
+side(chewbacca,1).
+side(finn,1).
+side(moff_gideon,0).
+side(grand_moff_tarkin,0).
+side(ahsoka_tano,1).
+side(mace_windu,1).
+side(admiral_akbar,1).
+side(mandalorian,1).
+
+class(luke_skywalker,1).
+class(c-зpo,5).
+class(r2-d2,5).
+class(princess_leia,2).
+class(yoda,1).
+class(darth_vader,0).
+class(darth_sidious,0).
+class(padme_amidalla,4).
+class(han_solo,2).
+class(anakin_skywalker,1).
+class(obi-wan_kenobi,1).
+class(rey,1).
+class(darth_maul,0).
+class(boba_fett,3).
+class(jango_fett,3).
+class(general_grievous,5).
+class(ja-ja_binks,5).
+class(chewbacca,2).
+class(finn,2).
+class(moff_gideon,4).
+class(grand_moff_tarkin,4).
+class(ahsoka_tano,1).
+class(mace_windu,1).
+class(admiral_akbar,2).
+class(mandalorian,3).
+
+power_ownership(luke_skywalker,1).
+power_ownership(c-зpo,0).
+power_ownership(r2-d2,0).
+power_ownership(princess_leia,0).
+power_ownership(yoda,1).
+power_ownership(darth_vader,1).
+power_ownership(darth_sidious,1).
+power_ownership(padme_amidalla,0).
+power_ownership(han_solo,0).
+power_ownership(anakin_skywalker,1).
+power_ownership(obi-wan_kenobi,1).
+power_ownership(rey,1).
+power_ownership(darth_maul,1).
+power_ownership(boba_fett,0).
+power_ownership(jango_fett,0).
+power_ownership(general_grievous,0).
+power_ownership(ja-ja_binks,0).
+power_ownership(chewbacca,0).
+power_ownership(finn,0).
+power_ownership(moff_gideon,0).
+power_ownership(grand_moff_tarkin,0).
+power_ownership(ahsoka_tano,1).
+power_ownership(mace_windu,1).
+power_ownership(admiral_akbar,0).
+power_ownership(mandalorian,0).
+
+possession_of_weapons(luke_skywalker,1).
+possession_of_weapons(c-зpo,0).
+possession_of_weapons(r2-d2,0).
+possession_of_weapons(princess_leia,2).
+possession_of_weapons(yoda,1).
+possession_of_weapons(darth_vader,1).
+possession_of_weapons(darth_sidious,1).
+possession_of_weapons(padme_amidalla,2).
+possession_of_weapons(han_solo,2).
+possession_of_weapons(anakin_skywalker,1).
+possession_of_weapons(obi-wan_kenobi,1).
+possession_of_weapons(rey,1).
+possession_of_weapons(darth_maul,1).
+possession_of_weapons(boba_fett,2).
+possession_of_weapons(jango_fett,2).
+possession_of_weapons(general_grievous,1).
+possession_of_weapons(ja-ja_binks,0).
+possession_of_weapons(chewbacca,2).
+possession_of_weapons(finn,1).
+possession_of_weapons(moff_gideon,1).
+possession_of_weapons(grand_moff_tarkin,0).
+possession_of_weapons(ahsoka_tano,1).
+possession_of_weapons(mace_windu,1).
+possession_of_weapons(admiral_akbar,0).
+possession_of_weapons(mandalorian,2).
+
+color_of_sword(luke_skywalker,1).
+color_of_sword(c-зpo,3).
+color_of_sword(r2-d2,3).
+color_of_sword(princess_leia,3).
+color_of_sword(yoda,1).
+color_of_sword(darth_vader,2).
+color_of_sword(darth_sidious,2).
+color_of_sword(padme_amidalla,3).
+color_of_sword(han_solo,3).
+color_of_sword(anakin_skywalker,0).
+color_of_sword(obi-wan_kenobi,0).
+color_of_sword(rey,0).
+color_of_sword(darth_maul,2).
+color_of_sword(boba_fett,3).
+color_of_sword(jango_fett,3).
+color_of_sword(general_grievous,3).
+color_of_sword(ja-ja_binks,3).
+color_of_sword(chewbacca,3).
+color_of_sword(finn,0).
+color_of_sword(moff_gideon,3).
+color_of_sword(grand_moff_tarkin,3).
+color_of_sword(ahsoka_tano,1).
+color_of_sword(mace_windu,3).
+color_of_sword(admiral_akbar,3).
+color_of_sword(mandalorian,3).
+
+number_of_swords(luke_skywalker,1).
+number_of_swords(c-зpo,0).
+number_of_swords(r2-d2,0).
+number_of_swords(princess_leia,0).
+number_of_swords(yoda,1).
+number_of_swords(darth_vader,1).
+number_of_swords(darth_sidious,1).
+number_of_swords(padme_amidalla,0).
+number_of_swords(han_solo,0).
+number_of_swords(anakin_skywalker,1).
+number_of_swords(obi-wan_kenobi,1).
+number_of_swords(rey,1).
+number_of_swords(darth_maul,1).
+number_of_swords(boba_fett,0).
+number_of_swords(jango_fett,0).
+number_of_swords(general_grievous,2).
+number_of_swords(ja-ja_binks,0).
+number_of_swords(chewbacca,0).
+number_of_swords(finn,1).
+number_of_swords(moff_gideon,1).
+number_of_swords(grand_moff_tarkin,0).
+number_of_swords(ahsoka_tano,2).
+number_of_swords(mace_windu,1).
+number_of_swords(admiral_akbar,0).
+number_of_swords(mandalorian,0).
+
+padawan(luke_skywalker,3).
+padawan(c-зpo,0).
+padawan(r2-d2,0).
+padawan(princess_leia,0).
+padawan(yoda,2).
+padawan(darth_vader,2).
+padawan(darth_sidious,2).
+padawan(padme_amidalla,0).
+padawan(han_solo,0).
+padawan(anakin_skywalker,1).
+padawan(obi-wan_kenobi,2).
+padawan(rey,1).
+padawan(darth_maul,1).
+padawan(boba_fett,0).
+padawan(jango_fett,0).
+padawan(general_grievous,0).
+padawan(ja-ja_binks,0).
+padawan(chewbacca,0).
+padawan(finn,0).
+padawan(moff_gideon,0).
+padawan(grand_moff_tarkin,0).
+padawan(ahsoka_tano,3).
+padawan(mace_windu,2).
+padawan(admiral_akbar,0).
+padawan(mandalorian,0).
+
+betrayer(luke_skywalker,0).
+betrayer(c-зpo,0).
+betrayer(r2-d2,0).
+betrayer(princess_leia,0).
+betrayer(yoda,0).
+betrayer(darth_vader,1).
+betrayer(darth_sidious,0).
+betrayer(padme_amidalla,0).
+betrayer(han_solo,0).
+betrayer(anakin_skywalker,1).
+betrayer(obi-wan_kenobi,0).
+betrayer(rey,0).
+betrayer(darth_maul,0).
+betrayer(boba_fett,0).
+betrayer(jango_fett,0).
+betrayer(general_grievous,0).
+betrayer(ja-ja_binks,0).
+betrayer(chewbacca,0).
+betrayer(finn,0).
+betrayer(moff_gideon,0).
+betrayer(grand_moff_tarkin,0).
+betrayer(ahsoka_tano,0).
+betrayer(mace_windu,0).
+betrayer(admiral_akbar,0).
+betrayer(mandalorian,0).
+
+translate(luke_skywalker,0).
+translate(c-зpo,1).
+translate(r2-d2,0).
+translate(princess_leia,0).
+translate(yoda,0).
+translate(darth_vader,0).
+translate(darth_sidious,0).
+translate(padme_amidalla,0).
+translate(han_solo,0).
+translate(anakin_skywalker,0).
+translate(obi-wan_kenobi,0).
+translate(rey,0).
+translate(darth_maul,0).
+translate(boba_fett,0).
+translate(jango_fett,0).
+translate(general_grievous,0).
+translate(ja-ja_binks,0).
+translate(chewbacca,0).
+translate(finn,0).
+translate(moff_gideon,0).
+translate(grand_moff_tarkin,0).
+translate(ahsoka_tano,0).
+translate(mace_windu,0).
+translate(admiral_akbar,0).
+translate(mandalorian,0).
+
+special_color(luke_skywalker,0).
+special_color(c-зpo,0).
+special_color(r2-d2,1).
+special_color(princess_leia,0).
+special_color(yoda,0).
+special_color(darth_vader,0).
+special_color(darth_sidious,0).
+special_color(padme_amidalla,0).
+special_color(han_solo,0).
+special_color(anakin_skywalker,0).
+special_color(obi-wan_kenobi,0).
+special_color(rey,0).
+special_color(darth_maul,0).
+special_color(boba_fett,0).
+special_color(jango_fett,1).
+special_color(general_grievous,0).
+special_color(ja-ja_binks,0).
+special_color(chewbacca,0).
+special_color(finn,0).
+special_color(moff_gideon,0).
+special_color(grand_moff_tarkin,0).
+special_color(ahsoka_tano,0).
+special_color(mace_windu,0).
+special_color(admiral_akbar,0).
+special_color(mandalorian,1).
+
+
+question1(X1):- write("Кем является ваш персонаж?"),nl,
+    write("2. Существо"),nl,
+    write("1. Дроид"),nl,
+    write("0. Человек"),nl,
+    read(X1).
+
+question2(X2):- write("На какой стороне ваш персонаж?"),nl,
+    write("1. Светлой"),nl,
+    write("0. Тёмной"),nl,
+    read(X2).
+
+question3(X3):- write("К какому классу, из перечисленных, относится ваш персонаж?"),nl,
+    write("5. Другие"),nl,
+    write("4. Клоны"),nl,
+    write("3. Охотники за головами"),nl,
+    write("2. Повстанцы"),nl,
+    write("1. Джедаи"),nl,
+    write("0. Ситхи"),nl,
+    read(X3).
+
+question4(X4):- write("Управляет ли ваш персонаж силой?"),nl,
+    write("1. Да"),nl,
+    write("0. Нет"),nl,
+    read(X4).
+
+question5(X5):- write("Владеет ли ваш персонаж орудием?"),nl,
+    write("2. Бластером"),nl,
+    write("1. Мечом"),nl,
+    write("0. Нет"),nl,
+    read(X5).
+
+question6(X6):- write("Каким цветом светового меча обладает ваш персонаж?"),nl,
+    write("3. Другой"),nl,
+    write("2. Красный"),nl,
+    write("1. Зелёный"),nl,
+    write("0. Синий"),nl,
+    read(X6).
+
+question7(X7):- write("Сколькими световыми мечами владеет ваш персонаж?"),nl,
+    write("2. Больше одного"),nl,
+    write("1. Одним"),nl,
+    write("0. Ни одним"),nl,
+    read(X7).
+
+question8(X8):- write("Ваш персонаж падаван или магистр?"),nl,
+    write("3. Оба варианта"),nl,
+    write("2. Магистр"),nl,
+    write("1. Падаван"),nl,
+    write("0. Никто"),nl,
+    read(X8).
+
+question9(X9):- write("Является ли ваш персонаж предателем?"),nl,
+    write("1. Да"),nl,
+    write("0. Нет"),nl,
+    read(X9).
+
+question10(X10):- write("Ваш персонаж владеет многими языками?"),nl,
+    write("1. Да"),nl,
+    write("0. Нет"),nl,
+    read(X10).
+
+question11(X11):- write("Во внешности вашего персонажа преобладают следующие цвета?"),nl,
+    write("2. Оранжевый"),nl,
+    write("1. Синий"),nl,
+    write("0. Не преобладают"),nl,
+    read(X11).
+
+play :- question1(X1),question2(X2),question3(X3),question4(X4),
+    question5(X5),question6(X6),question7(X7),question8(X8),
+    question9(X9),question10(X10),question11(X11),
+    character(X,X1),side(X,X2),class(X,X3),power_ownership(X,X4),possession_of_weapons(X,X5),color_of_sword(X,X6),number_of_swords(X,X7),padawan(X,X8),
+   betrayer(X,X9),translate(X,X10),special_color(X,X11),
+    write(X).
+
+
