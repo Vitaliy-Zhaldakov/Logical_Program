1055c6fcbaffa7b7a1f49da3d98c6d48fc7c1190
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-03-05 16:51:27 +0300

    Task 12

--- Laboratorka 4/Lab_4.pl
+++ Laboratorka 4/Lab_4.pl
@@ -46,3 +46,7 @@ reverse_list([H|T],T1,Revl):- reverse_list(T,[H|T1],Revl).
 
 p([],_):-!.
 p([SubH|SubT],[H|T]):- H = SubH -> p(SubT,T) ; p([SubH|SubT],T).
+
+delete_elem(List,Number,Result):- delete_elem(List,Number,0,Result).
+delete_elem([_|T],Number,Number, T):-!.
+delete_elem([H|T],Number, I,[H|T1]):- I1 is I + 1, delete_elem(T,Number,I1,T1).
