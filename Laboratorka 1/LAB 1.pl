6090833f3bc6c33a7f2dec475de3d2e2e89b3e2f
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2021-03-16 23:26:45 +0300

    Комментирование LAB 1

--- Laboratorka 1/LAB 1.pl
+++ Laboratorka 1/LAB 1.pl
@@ -1,3 +1,4 @@
+% Мужчины:
 man(vladimir).
 man(oleg).
 man(yuri).
@@ -7,6 +8,7 @@ man(denis).
 man(alexey).
 man(alexandr).
 
+% Женщины:
 woman(ann).
 woman(angelina).
 woman(sofia).
@@ -16,6 +18,7 @@ woman(natalya).
 woman(maria).
 woman(julia).
 
+% Семьи:
 parent(vladimir,angelina).
 parent(vladimir,yuri).
 parent(vladimir,vladislav).
@@ -42,39 +45,53 @@ parent(victoria,maria).
 parent(victoria,alexander).
 parent(victoria,julia).
 
+% Task 1 Предикаты вывода всех мужчин и женщин
 man():-man(X),write(X),nl,fail.
 woman() :-woman(X),write(X),nl,fail.
+
+% Task 2 Предикат вывода всех детей 'X'
 children(X) :- parent(X,Y),write(Y),nl,fail.
 
+% Task 3 Предикаты проверки матери и вывод матери 'X'
 mother(X,Y) :- parent(X,Y),woman(X).
 mother(X) :- mother(Y,X),write(Y).
 
+% Task 4 Предикаты проверки сына и вывод сына 'X'
 son(X,Y) :- parent(Y,X),man(X).
 son(X) :- son(Y,X),write(Y).
 
+% Task 5 Предикаты проверки брата и вывод всех братьев 'X'
 brother(X,Y) :- mother(Z,X),mother(Z,Y),man(X),X\=Y.
 brothers(X) :- brother(Y,X),write(Y),nl,fail.
 
+% Task 6 Предикаты проверки мужа и вывод мужа 'X'
 husband(X,Y) :- parent(X,Z),parent(Y,Z),man(X),X\=Y.
 husband(X) :- husband(Y,X),write(Y).
 
+% Task 7 Предикаты проверки братьев и сестёр и вывод всех братьев и сестёр 'X'
 b_s(X,Y) :- mother(Z,X),mother(Z,Y),X\=Y.
 b_s(X) :- b_s(Y,X),write(Y),nl,fail.
 
+% Task 8 Предикаты проверки дедушки и вывод всех дедушек 'X'
 grand_pa(X,Y) :- parent(Z,Y),parent(X,Z),man(X).
 grand_pas(X) :- grand_pa(Y,X),write(Y),nl,fail.
 
+% Task 9 Предикаты проверки внука и вывод всех внуков 'X'
 grand_so(X,Y) :- parent(Z,X),parent(Y,Z),man(X).
 grand_sons(X) :- grand_so(Y,X),write(Y),nl,fail.
 
+% Task 10 Предикаты проверки дедушки и внука
 grand_pa_and_son(X,Y) :- grand_pa(X,Y),grand_so(Y,X).
 grand_pa_and_son(X,Y) :- grand_pa(Y,X),grand_so(X,Y).
 
+% Task 11 Предикаты проверки дедушки и внучки
 grand_pa_and_da(X,Y) :- grand_pa(X,Y),parent(Z,Y),parent(X,Z),woman(Y).
 grand_pa_and_da(X,Y) :- grand_pa(Y,X),parent(Z,X),parent(Y,Z),woman(X).
 
+% Task 12 Предикаты проверки дяди и вывод всех дядей 'X'
 uncle(X,Y) :- parent(Z,Y),brother(X,Z).
 uncles(X) :- uncle(Y,X),write(Y),nl,fail.
 
+% Task 13 Предикаты проверки племянницы и вывод всех племянниц 'X'
 niece(X,Y) :- uncle(Y,X),woman(X).
 nieces(X) :- niece(Y,X),write(Y),nl,fail.
